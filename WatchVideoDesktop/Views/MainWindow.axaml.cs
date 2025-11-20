d180b2dcc1813a8f45f76be94d657575807b7ce6
wolanski_mateusz <94111142+wolanskimateusz@users.noreply.github.com>
2025-11-20 15:54:02 +0100

    init

---
+++ WatchVideoDesktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WatchVideoDesktop.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
