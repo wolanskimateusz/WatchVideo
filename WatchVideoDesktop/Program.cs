c6eafcce1f795dec25ef76f9b8ab1d02660a6848
wolanski_mateusz <94111142+wolanskimateusz@users.noreply.github.com>
2025-12-02 14:43:53 +0100

    rename

--- WatchVideoDesktop/Program.cs
+++ WatchVideoDesktop/Program.cs
@@ -1,9 +1,16 @@
 ﻿using Avalonia;
+<<<<<<< Updated upstream
 using LibVLCSharp.Shared;
 using WatchVideoDesktop;
+=======
+using System;
+using LibVLCSharp.Shared;
+
+>>>>>>> Stashed changes
 
 internal class Program
 {
+<<<<<<< Updated upstream
     public static void Main(string[] args)
     {
         // Inicjalizacja biblioteki LibVLC
@@ -13,6 +20,23 @@ internal class Program
             .StartWithClassicDesktopLifetime(args);
     }
 
+=======
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        
+        Core.Initialize();
+        
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+    
+    
+    // Avalonia configuration, don't remove; also used by visual designer.
+>>>>>>> Stashed changes
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
--- WatchVideoDesktop/Views/MainWindow.axaml.cs
+++ WatchVideoDesktop/Views/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
+using System;
 using Avalonia.Controls;
 using LibVLCSharp.Shared;
+<<<<<<< Updated upstream
 namespace WatchVideoDesktop;
 
 public partial class MainWindow : Window
@@ -7,10 +9,27 @@ public partial class MainWindow : Window
     private LibVLC _libVLC;
     private MediaPlayer _mediaPlayer;
 
+=======
+
+namespace WatchVideoDesktop.Views;
+
+public partial class MainWindow : Window
+{
+    
+    private LibVLC _libVLC;
+    private MediaPlayer _mediaPlayer;
+
+    // Link do osadzenia (embed) filmu YouTube. 
+    // Zastąp "TWOJ_ID_FILMU" właściwym ID.
+    private const string YouTubeLink = "https://www.youtube.com/watch?v=fXA0I6eyW2E";
+    
+    
+>>>>>>> Stashed changes
     public MainWindow()
     {
         InitializeComponent();
 
+<<<<<<< Updated upstream
         _libVLC = new LibVLC();
         _mediaPlayer = new MediaPlayer(_libVLC);
 
@@ -21,5 +40,49 @@ public partial class MainWindow : Window
 
         var media = new Media(_libVLC, url, FromType.FromLocation);
         _mediaPlayer.Play(media);
+=======
+        InitializeVLCPlayer();
+    }
+    
+    private void InitializeVLCPlayer()
+    {
+        string[] options = new string[]
+        {
+            "--verbose=2",        // Więcej logów, które mogą pomóc w diagnozie
+            "--aout=directsound", // Ustawia sterownik audio
+            "--vout=directx" ,     // Ustawia sterownik wideo na DirectX (typowy dla Windows)
+            
+            "--http-caching=3000",
+            "--network-caching=3000",
+        
+            // Debugowanie Lua
+            "--lua-config=script-logging=true"
+        };
+        
+        // Utwórz instancję LibVLC
+        _libVLC = new LibVLC(options);
+
+        // Utwórz Media Player
+        _mediaPlayer = new MediaPlayer(_libVLC);
+
+        // Powiąż odtwarzacz z kontrolką VideoView z XAML
+        PlayerView.MediaPlayer = _mediaPlayer;
+
+        // Utwórz obiekt Media z linkiem do YouTube (LibVLC sam obsłuży strumień)
+        var media = new Media(_libVLC, YouTubeLink, FromType.FromLocation);
+
+        // Rozpocznij odtwarzanie
+        _mediaPlayer.Play(media);
+    }
+
+    // WAŻNE: Zwolnienie zasobów przy zamykaniu okna
+    protected override void OnClosed(EventArgs e)
+    {
+        base.OnClosed(e);
+
+        // Zwolnij Media Player, a następnie LibVLC
+        _mediaPlayer?.Dispose();
+        _libVLC?.Dispose();
+>>>>>>> Stashed changes
     }
 }
